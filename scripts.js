0bdb5a9113d1b851045e9cb0b1392c6d3fd4a4d0
AgusBermeo <agustinbermeo@gmail.com>
2023-11-05 08:50:40 -0500

    sumArrays script

--- scripts.js
+++ scripts.js
@@ -1,17 +1,13 @@
-function squareSum(numbers){
-    let sumSqrNum = 0;
+// Write a function that takes an array of numbers and returns the sum of the numbers. 
+// The numbers can be negative or non-integer. 
+// If the array does not contain any numbers then you should return 0.
 
-    /*
-    for (let i = 0; i < numbers.length; i++) {
-        sumSqrNum += numbers[i];
-    }
-    return sumSqrNum;
-    */
-
-
-    for (let element of numbers) {
-      let powElement = Math.pow(element, 2);;
-      sumSqrNum += powElement
-    }
-    return sumSqrNum;
-}
+// Sum Numbers
+function sum(numbers) {
+    "use strict";
+    let sumNumbers = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sumNumbers += numbers[i];
+  }
+    return sumNumbers;
+};
